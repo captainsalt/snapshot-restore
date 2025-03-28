1cd5f21ff6791422edb965adb0eba4397273eb0c
captainsalt <dontbothertheking@gmail.com>
2025-03-27 20:51:49 -0500

    Add endpoint url

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 mod cli;
 use aws_authentication::*;
 use aws_ec2::find_instances_by_name;
-use aws_sdk_ec2::client;
+use aws_sdk_ec2::{client, config};
 use clap::Parser;
 use cli::Args;
 use tokio::time::error::Error;
@@ -12,7 +12,11 @@ mod aws_ec2;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::parse();
-    let config = get_config(Some(&args.profile)).await;
+    let config = get_config(Some(&args.profile))
+        .await
+        .to_builder()
+        .endpoint_url("")
+        .build();
 
     let ec2_client = client::Client::new(&config);
     let instances = find_instances_by_name(&ec2_client, vec!["dw-instance-0"]).await;
