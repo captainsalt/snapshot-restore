a0e5197511fc161073790aab20325a18350b80a2
David B. Wright <david.b.wright@afs.com>
2025-03-27 19:55:18 -0500

    Working out bugs

--- src/aws_ec2/mod.rs
+++ src/aws_ec2/mod.rs
@@ -35,47 +35,47 @@ pub async fn find_instances_by_id(ec2_client: &Client, instance_ids: Vec<String>
 
 pub async fn find_instances_by_name(
     ec2_client: &Client,
-    instance_names: Vec<String>,
+    instance_names: Vec<&str>,
 ) -> Vec<Instance> {
     get_instances(
         ec2_client,
         Some(vec![
             Filter::builder()
                 .name("tag:Name")
-                .set_values(Some(instance_names))
+                .set_values(Some(instance_names.iter().map(|s| s.to_string())))
                 .build(),
         ]),
     )
     .await
 }
 
-pub async fn stop_instance(ec2_client: &Client, instance_id: &String) {
+pub async fn stop_instance(ec2_client: &Client, instance: Instance) {
     ec2_client
         .stop_instances()
-        .instance_ids(instance_id)
+        .instance_ids(instance.instance_id.as_deref().unwrap_or_default())
         .send()
         .await
         .expect("Error stopping instances");
 
     ec2_client
         .wait_until_instance_stopped()
-        .instance_ids(instance_id)
+        .instance_ids(instance.instance_id.as_deref().unwrap_or_default())
         .wait(Duration::from_secs(600_000))
         .await
         .expect("Error waiting for instances to stop");
 }
 
-pub async fn start_instance(ec2_client: &Client, insatnce_id: &String) {
+pub async fn start_instance(ec2_client: &Client, instance: Instance) {
     ec2_client
         .start_instances()
-        .instance_ids(insatnce_id)
+        .instance_ids(instance.instance_id.as_deref().unwrap_or_default())
         .send()
         .await
         .expect("Error starting instances");
 
     ec2_client
         .wait_until_instance_status_ok()
-        .instance_ids(insatnce_id)
+        .instance_ids(instance.instance_id.as_deref().unwrap_or_default())
         .wait(Duration::from_secs(600_000))
         .await
         .expect("Error waiting for instances to start");
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod cli;
 use aws_authentication::*;
+use aws_ec2::find_instances_by_name;
+use aws_sdk_ec2::client;
 use clap::Parser;
 use cli::Args;
 use tokio::time::error::Error;
@@ -9,7 +11,24 @@ mod aws_ec2;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::parse();
-    let profile = get_config(Some(&args.profile)).await;
+    let config = get_config(Some(&args.profile)).await;
+
+    let ec2_client = client::Client::new(&config);
+    let instances = find_instances_by_name(&ec2_client, vec!["dw-instance-0"]).await;
+
+    print!(
+        "Found instance {}",
+        instances
+            .first()
+            .expect("No instances found")
+            .tags
+            .expect("No tags found")
+            .iter()
+            .find(|t| t.key.as_deref().unwrap_or_default() == "Name")
+            .unwrap()
+            .value
+            .unwrap_or_default()
+    );
 
     Ok(())
 }
