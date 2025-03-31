b4a5385cda0fec7cd573d94d07b6fa8a934faa45
captainsalt <dontbothertheking@gmail.com>
2025-03-31 14:01:08 -0500

    Make a TODO file

---
+++ TODO.md
@@ -0,0 +1,3 @@
+Get default region from config file
+Fail if profile isn't found
+Look in multiple regions
--- src/aws_authentication/mod.rs
+++ src/aws_authentication/mod.rs
@@ -1,9 +1,6 @@
 use aws_config::meta::region::RegionProviderChain;
 use aws_config::{BehaviorVersion, SdkConfig};
 
-// TODO: Get default region from config file
-// TODO: Fail if profile isn't found
-// TODO: Look in multiple regions
 pub async fn get_profile(profile: Option<&str>) -> SdkConfig {
     let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
 
