c8409a74c2728612c6a7dcf6f925c544a6f1a92a
captainsalt <dontbothertheking@gmail.com>
2025-04-03 15:38:14 -0500

    Update main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod aws_ec2;
 mod cli_args;
 use aws_authentication::*;
 use aws_config::SdkConfig;
-use aws_ec2::{find_instances_by_name, get_instance_snapshots, get_most_recent_snapshots};
+use aws_ec2::{
+    app_err::ApplicationError, find_instances_by_name, get_instance_snapshots,
+    get_most_recent_snapshots,
+};
 use clap::Parser;
 use cli_args::Args;
 use config::Config;
@@ -39,16 +42,17 @@ fn read_instance_names(input_file_path: &String) -> Result<Vec<String>, std::io:
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), ApplicationError> {
     let args = Args::parse();
     let app_config = get_app_config();
     let aws_profile = get_profile(Some(&args.profile)).await;
     let ec2_client = create_ec2_client(&app_config, &aws_profile);
 
-    let instance_names = read_instance_names(&args.instance_file);
-    let instances = find_instances_by_name(&ec2_client, instance_names.unwrap())
+    let instance_names = read_instance_names(&args.instance_file)
+        .map_err(|err| ApplicationError::from_err("Error reading instances from file", err))?;
+    let instances = find_instances_by_name(&ec2_client, instance_names)
         .await
-        .expect("Error getting instances");
+        .map_err(|err| ApplicationError::from_err("Could not find instances provided", err))?;
 
     let instance = instances.first().expect("Should be at least one instance");
     let snapshots = get_instance_snapshots(&ec2_client, instance).await;
