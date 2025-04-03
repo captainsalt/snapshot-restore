2b3fa265fbcec019f278972301b08e4eabd35636
captainsalt <dontbothertheking@gmail.com>
2025-04-03 15:46:22 -0500

    Update app_err

--- src/aws_ec2/app_err.rs
+++ src/aws_ec2/app_err.rs
@@ -2,21 +2,18 @@ use aws_sdk_ec2::error::SdkError;
 use std::error::Error as StdError;
 use std::fmt::{self};
 
-// A simple error type that just contains a message
 #[derive(Debug)]
 pub struct ApplicationError {
     message: String,
 }
 
 impl ApplicationError {
-    // Create a new error with a message
     pub fn new<S: Into<String>>(message: S) -> Self {
         ApplicationError {
             message: message.into(),
         }
     }
 
-    // Create an error from another error
     pub fn from_err<E: std::fmt::Debug>(message: &str, err: E) -> Self {
         ApplicationError {
             message: format!("{}: {:?}", message, err),
@@ -24,7 +21,6 @@ impl ApplicationError {
     }
 }
 
-// Required for custom error types
 impl fmt::Display for ApplicationError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.message)
@@ -33,9 +29,8 @@ impl fmt::Display for ApplicationError {
 
 impl StdError for ApplicationError {}
 
-// Helper function for any SDK error
-impl<E> From<SdkError<E>> for ApplicationError {
+impl<E: std::fmt::Debug> From<SdkError<E>> for ApplicationError {
     fn from(err: SdkError<E>) -> Self {
-        ApplicationError::new(format!("AWS SDK error: {}", err))
+        ApplicationError::new(format!("AWS SDK error: {:?}", err))
     }
 }
