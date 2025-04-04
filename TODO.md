75f9d0ce9eb4b9de880e710962a0e1896eb3f40f
captainsalt <dontbothertheking@gmail.com>
2025-04-04 15:30:48 -0500

    Update todo

--- TODO.md
+++ TODO.md
@@ -1,5 +1,2 @@
-Specify region in CLI
-get_instance_snapshots Should filter by tag name
 Investigate the rust JSON
-Fail if profile isn't found
 Look in multiple regions for instances
--- src/aws/authentication.rs
+++ src/aws/authentication.rs
@@ -4,9 +4,13 @@ use aws_config::{BehaviorVersion, SdkConfig};
 pub async fn get_profile(profile: Option<&str>) -> SdkConfig {
     let region_provider = RegionProviderChain::default_provider();
 
+    if let None = profile {
+        panic!("Profile not specified")
+    }
+
     aws_config::defaults(BehaviorVersion::latest())
         .region(region_provider)
-        .profile_name(profile.unwrap_or("default"))
+        .profile_name(profile.unwrap())
         .load()
         .await
 }
